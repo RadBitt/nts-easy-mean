2815162515c790c8df1aee27d007167ea0624d68
radbitt <leo@radbitt.com>
2016-10-05 15:59:33 -0700

    bower

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-        1 file(s) copied.
+'use strict';
+
+var express = require('express'); 
+
+var app = express();
+
+app.use('/', express.static('public'));
+
+app.listen(3000, function() {
+	console.log('Server is running on port 3000');
+})
