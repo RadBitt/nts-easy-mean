fb7ba9370c1a48d8774b887a15f3c958b4008bc9
Leonardo <leo@radbitt.com>
2017-01-17 10:51:10 -0800

    Delete base.js

--- app/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-    apiKey: "AIzaSyD-ZmgYyQLWfuk028K0xo_lUjaGiB4BnG0",
-    authDomain: "nts-easy-mean.firebaseapp.com",
-    databaseURL: "https://nts-easy-mean.firebaseio.com",
- });
-
-export default base;
